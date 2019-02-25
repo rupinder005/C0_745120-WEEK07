b715fa075dfe0692223adf281356a5f0df63762b
visualstudio <visualstudio@230-PC21>
2019-02-25 14:42:10 -0500

    while loop

--- C0_745120-WEEK07/js/main.js
+++ C0_745120-WEEK07/js/main.js
@@ -1,1 +1,22 @@
 ﻿// Your code here!
+<html>
+    <head>
+    </head>
+    <body>
+        
+            function p1()
+            {
+
+          
+            sum=0;
+            for(i=117;i>=11;)
+            {
+                
+                sum += i;
+                i-=8;
+                return sum;
+}
+    }
+        
+    </body>
+</html>
